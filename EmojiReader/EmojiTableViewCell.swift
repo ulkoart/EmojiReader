5ef20bbdd1bcc2a072900c2e37e8b74c5ce2b3f0
ulkoart <ulkoart@gmail.com>
2020-04-28 16:17:39 +0300

    init table view

---
+++ EmojiReader/EmojiTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  EmojiTableViewCell.swift
+//  EmojiReader
+//
+//  Created by user on 28.04.2020.
+//  Copyright © 2020 ulkoart. All rights reserved.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
